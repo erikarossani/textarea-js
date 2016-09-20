d6c36a01f71a877df2eb9b21c49c45049966b831
yeiny gonzalez cifuentes <yeinygonzalezcifuentes@gmail.com>
2016-09-20 11:42:25 -0500

    contador

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ window.addEventListener("load", function() {
 	var boton = document.getElementById("resultado");
   boton.disabled =true;
   var textArea = document.getElementById("texto");
+  var contar = document.getElementById("contar");
 
 	boton.addEventListener("click", function(e){
 		e.preventDefault();
@@ -14,14 +15,22 @@ window.addEventListener("load", function() {
     });  
     function agregarMensaje(texto){
         var nuevoItem = document.createElement("div");
-        nuevoItem.classList.add("texto")
+        nuevoItem.classList.add("texto");
         nuevoItem.innerHTML =texto;
 
        var lista = document.getElementById("comentario");
        lista.insertBefore(nuevoItem, comentario.childNodes[0]);
        document.getElementById("texto").value = "";
     }  
+
     textArea.addEventListener("keydown", function(){
       boton.disabled=false;
+
      });
+    textArea.addEventListener("keyup", function(){
+      var longitud = 140;
+      var caracter = textArea.value.length;
+      contar.innerText = longitud - caracter;
+     });
+
 });
