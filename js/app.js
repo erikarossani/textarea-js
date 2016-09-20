8f8d5e041a935d0804142685d379012a6dbf9f6f
yeiny gonzalez cifuentes <yeinygonzalezcifuentes@gmail.com>
2016-09-20 09:08:14 -0500

    habilitando y desahabilitar boton

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
 window.addEventListener("load", function() {
 	var boton = document.getElementById("resultado");
-	boton.addEventListener("click", function() {
-		var texto = document.getElementById("texto").value;
+  boton.disabled =true;
+  var textArea = document.getElementById("texto");
 
+	boton.addEventListener("click", function(e){
+		e.preventDefault();
+		var texto = textArea.value;
+            agregarMensaje(texto);
+            textArea.value ="";
+            boton.disabled= true;
+
+
+    });  
+    function agregarMensaje(texto){
         var nuevoItem = document.createElement("div");
         nuevoItem.classList.add("texto")
         nuevoItem.innerHTML =texto;
@@ -10,5 +20,8 @@ window.addEventListener("load", function() {
        var lista = document.getElementById("comentario");
        lista.insertBefore(nuevoItem, comentario.childNodes[0]);
        document.getElementById("texto").value = "";
-    });
+    }  
+    textArea.addEventListener("keydown", function(){
+      boton.disabled=false;
+     });
 });
